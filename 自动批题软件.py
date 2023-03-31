8074c670242a778f1e75c9d9632c448caa91d009
zzy13335040016 <93032374+zzy13335040016@users.noreply.github.com>
2023-03-31 22:34:25 +0800

    Update 自动批题软件.py

--- 自动批题软件.py
+++ 自动批题软件.py
@@ -37,10 +37,10 @@ k = Controller()  # 设置控制键盘的变量
 
 # 定义其他函数
 def expand():  # 扩展函数,去github获取文件
-    tk_expand = tkinter.Toplevel()  # 此版本没有此功能，仅创建一个窗口
+    tk_expand = tkinter.Toplevel()  # 此版本没有此功能，仅创建一个窗口，1.x.x.x版本更新扩展
     tk_expand.title('扩展(实验性)')
     tk_expand_label = tkinter.Label(
-        tk_expand, text='此版本暂无扩展,请等待下个版本发布', font=('等线', 15))
+        tk_expand, text='此版本暂无扩展,请等待1.x.x.x版本发布', font=('等线', 15))
     tk_expand_label.grid(row=0, column=0)
     tk_expand_button = tkinter.ttk.Button(
         tk_expand, text='关闭', command=tk_expand.destroy)
